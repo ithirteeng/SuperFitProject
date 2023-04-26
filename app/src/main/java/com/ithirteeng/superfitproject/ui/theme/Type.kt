cecbb911a56bc06e1384ad8d57b29b25d36422b8
Gulevskii Ivan <ithirteengw@gmail.com>
2023-04-26 18:34:46 +0700

    add new fonts and setup typography

--- app/src/main/java/com/ithirteeng/superfitproject/ui/theme/Type.kt
+++ app/src/main/java/com/ithirteeng/superfitproject/ui/theme/Type.kt
@@ -2,27 +2,54 @@ package com.ithirteeng.superfitproject.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.ithirteeng.superfitproject.R
 
-// Set of Material typography styles to start with
+val Montserrat = FontFamily(
+    Font(R.font.motserrat_400_regular, FontWeight(400)),
+    Font(R.font.montserrat_700_bold, FontWeight(700)),
+    Font(R.font.montserrat_300_light, FontWeight(300))
+)
+
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
+    h1 = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight(700),
+        fontSize = 64.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight(300),
+        fontSize = 64.sp
+    ),
+    h4 = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight(700),
+        fontSize = 36.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight(700),
+        fontSize = 24.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight(400),
+        fontSize = 18.sp
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight(700),
         fontSize = 14.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+    body1 = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight(400),
         fontSize = 12.sp
     )
-    */
+
+
 )
