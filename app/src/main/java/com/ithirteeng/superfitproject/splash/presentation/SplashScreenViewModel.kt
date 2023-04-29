68c18a5587817253c6a1fe32d908902a606816ef
Gulevskii Ivan <ithirteengw@gmail.com>
2023-04-29 20:45:45 +0700

    Add logic of navigation from splash screen

--- app/src/main/java/com/ithirteeng/superfitproject/splash/presentation/SplashScreenViewModel.kt
+++ app/src/main/java/com/ithirteeng/superfitproject/splash/presentation/SplashScreenViewModel.kt
@@ -4,12 +4,15 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ithirteeng.superfitproject.common.token.domain.usecase.GetCurrentUserNameUseCase
 import com.ithirteeng.superfitproject.splash.presentation.model.CompletionModel
 import com.ithirteeng.superfitproject.splash.presentation.model.SplashNextScreenType
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class SplashScreenViewModel : ViewModel() {
+class SplashScreenViewModel(
+    private val getCurrentUserNameUseCase: GetCurrentUserNameUseCase,
+) : ViewModel() {
 
     private val _state = MutableLiveData(SplashState())
 
@@ -28,12 +31,23 @@ class SplashScreenViewModel : ViewModel() {
         )
         viewModelScope.launch {
             delay(1000)
-            _state.value = _state.value?.copy(
-                completionModel = CompletionModel(
-                    true,
-                    SplashNextScreenType.LOGIN
+            val userName = getCurrentUserNameUseCase()
+            if (userName == null) {
+                _state.value = _state.value?.copy(
+                    completionModel = CompletionModel(
+                        true,
+                        SplashNextScreenType.LOGIN
+                    )
                 )
-            )
+            } else {
+                _state.value = _state.value?.copy(
+                    completionModel = CompletionModel(
+                        true,
+                        SplashNextScreenType.LOGIN2
+                    ),
+                    userEmail = userName
+                )
+            }
         }
 
     }
